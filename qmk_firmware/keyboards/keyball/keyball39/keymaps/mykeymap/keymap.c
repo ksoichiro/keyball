06c2bf84c9d0bea0655bc0f94e96cc1873a8f617
Soichiro Kashima <soichiro.kashima@gmail.com>
2023-10-22 23:54:56 +0900

    Disable tap dance and enable combo

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/mykeymap/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/mykeymap/keymap.c
@@ -20,132 +20,143 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-typedef enum {
-    TD_NONE,
-    TD_UNKNOWN,
-    TD_SINGLE_TAP,
-    TD_SINGLE_HOLD,
-    TD_DOUBLE_TAP,
-    TD_DOUBLE_HOLD,
-    TD_DOUBLE_SINGLE_TAP, // Send two single taps
-    TD_TRIPLE_TAP,
-    TD_TRIPLE_HOLD
-} td_state_t;
-
-typedef struct {
-    bool is_press_action;
-    td_state_t state;
-} td_tap_t;
-
-// Tap dance enums
-enum {
-  TD_KANA_HAEN,
-  TD_TAB,
+const uint16_t PROGMEM as_combo[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM lmin_combo[] = {KC_L, KC_MINS, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(as_combo, KC_LNG2),
+    COMBO(df_combo, KC_LNG1),
+    COMBO(jk_combo, KC_BSPC),
+    COMBO(lmin_combo, KC_DEL),
 };
 
-td_state_t cur_dance(tap_dance_state_t *state);
-
-// For the x tap dance. Put it here so it can be used in any keymap
-void kana_finished(tap_dance_state_t *state, void *user_data);
-void kana_reset(tap_dance_state_t *state, void *user_data);
-void tab_finished(tap_dance_state_t *state, void *user_data);
-void tab_reset(tap_dance_state_t *state, void *user_data);
-
-td_state_t cur_dance(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        // Prefer to send a 'HOLD' over a 'SINGLE_TAP' if the key has been interrupted.
-        // This is the same as HOLD_ON_OTHER_KEY_PRESSED.
-        if (state->interrupted) return TD_SINGLE_HOLD;
-        if (!state->pressed) return TD_SINGLE_TAP;
-        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
-        else return TD_SINGLE_HOLD;
-    } else if (state->count == 2) {
-        // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
-        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
-        // keystrokes of the key, and not the 'double tap' action/macro.
-        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return TD_DOUBLE_HOLD;
-        else return TD_DOUBLE_TAP;
-    }
-
-    // Assumes no one is trying to type the same letter three times (at least not quickly).
-    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
-    // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
-    if (state->count == 3) {
-        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
-        else return TD_TRIPLE_HOLD;
-    } else return TD_UNKNOWN;
-}
-
-// Create an instance of 'td_tap_t' for the 'x' tap dance.
-static td_tap_t kana_tap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
-};
-
-void kana_finished(tap_dance_state_t *state, void *user_data) {
-    kana_tap_state.state = cur_dance(state);
-    switch (kana_tap_state.state) {
-        case TD_SINGLE_TAP: register_code(KC_LNG1); break;
-        case TD_SINGLE_HOLD: layer_on(3); break;
-        case TD_DOUBLE_TAP: register_code(KC_LNG2); break;
-        case TD_DOUBLE_HOLD: register_code(KC_LSFT); break;
-        // Last case is for fast typing. Assuming your key is `f`:
-        // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
-        // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
-        // case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code(KC_X); break;
-        default: break;
-    }
-}
-
-void kana_reset(tap_dance_state_t *state, void *user_data) {
-    switch (kana_tap_state.state) {
-        case TD_SINGLE_TAP: unregister_code(KC_LNG1); break;
-        case TD_SINGLE_HOLD: layer_off(3); break;
-        case TD_DOUBLE_TAP: unregister_code(KC_LNG2); break;
-        case TD_DOUBLE_HOLD: unregister_code(KC_LSFT); break;
-        // case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_X); break;
-        default: break;
-    }
-    kana_tap_state.state = TD_NONE;
-}
-
-static td_tap_t tab_tap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
-};
-
-void tab_finished(tap_dance_state_t *state, void *user_data) {
-    tab_tap_state.state = cur_dance(state);
-    switch (tab_tap_state.state) {
-        case TD_SINGLE_TAP: register_code(KC_TAB); break;
-        case TD_SINGLE_HOLD: layer_on(1); break;
-        case TD_DOUBLE_TAP: register_code(KC_ESC); break;
-        case TD_DOUBLE_HOLD: register_code(KC_LCTL); break;
-        // Last case is for fast typing. Assuming your key is `f`:
-        // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
-        // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
-        // case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code(KC_X); break;
-        default: break;
-    }
-}
-
-void tab_reset(tap_dance_state_t *state, void *user_data) {
-    switch (tab_tap_state.state) {
-        case TD_SINGLE_TAP: unregister_code(KC_TAB); break;
-        case TD_SINGLE_HOLD: layer_off(1); break;
-        case TD_DOUBLE_TAP: unregister_code(KC_ESC); break;
-        case TD_DOUBLE_HOLD: unregister_code(KC_LCTL); break;
-        // case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_X); break;
-        default: break;
-    }
-    tab_tap_state.state = TD_NONE;
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_KANA_HAEN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, kana_finished, kana_reset),
-    [TD_TAB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tab_finished, tab_reset)
-};
+// typedef enum {
+//     TD_NONE,
+//     TD_UNKNOWN,
+//     TD_SINGLE_TAP,
+//     TD_SINGLE_HOLD,
+//     TD_DOUBLE_TAP,
+//     TD_DOUBLE_HOLD,
+//     TD_DOUBLE_SINGLE_TAP, // Send two single taps
+//     TD_TRIPLE_TAP,
+//     TD_TRIPLE_HOLD
+// } td_state_t;
+//
+// typedef struct {
+//     bool is_press_action;
+//     td_state_t state;
+// } td_tap_t;
+//
+// // Tap dance enums
+// enum {
+//   TD_KANA_HAEN,
+//   TD_TAB,
+// };
+//
+// td_state_t cur_dance(tap_dance_state_t *state);
+//
+// // For the x tap dance. Put it here so it can be used in any keymap
+// void kana_finished(tap_dance_state_t *state, void *user_data);
+// void kana_reset(tap_dance_state_t *state, void *user_data);
+// void tab_finished(tap_dance_state_t *state, void *user_data);
+// void tab_reset(tap_dance_state_t *state, void *user_data);
+//
+// td_state_t cur_dance(tap_dance_state_t *state) {
+//     if (state->count == 1) {
+//         // Prefer to send a 'HOLD' over a 'SINGLE_TAP' if the key has been interrupted.
+//         // This is the same as HOLD_ON_OTHER_KEY_PRESSED.
+//         if (state->interrupted) return TD_SINGLE_HOLD;
+//         if (!state->pressed) return TD_SINGLE_TAP;
+//         // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
+//         else return TD_SINGLE_HOLD;
+//     } else if (state->count == 2) {
+//         // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+//         // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+//         // keystrokes of the key, and not the 'double tap' action/macro.
+//         if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
+//         else if (state->pressed) return TD_DOUBLE_HOLD;
+//         else return TD_DOUBLE_TAP;
+//     }
+//
+//     // Assumes no one is trying to type the same letter three times (at least not quickly).
+//     // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+//     // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
+//     if (state->count == 3) {
+//         if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
+//         else return TD_TRIPLE_HOLD;
+//     } else return TD_UNKNOWN;
+// }
+//
+// // Create an instance of 'td_tap_t' for the 'x' tap dance.
+// static td_tap_t kana_tap_state = {
+//     .is_press_action = true,
+//     .state = TD_NONE
+// };
+//
+// void kana_finished(tap_dance_state_t *state, void *user_data) {
+//     kana_tap_state.state = cur_dance(state);
+//     switch (kana_tap_state.state) {
+//         case TD_SINGLE_TAP: register_code(KC_LNG1); break;
+//         case TD_SINGLE_HOLD: layer_on(3); break;
+//         case TD_DOUBLE_TAP: register_code(KC_LNG2); break;
+//         case TD_DOUBLE_HOLD: register_code(KC_LSFT); break;
+//         // Last case is for fast typing. Assuming your key is `f`:
+//         // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+//         // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+//         // case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code(KC_X); break;
+//         default: break;
+//     }
+// }
+//
+// void kana_reset(tap_dance_state_t *state, void *user_data) {
+//     switch (kana_tap_state.state) {
+//         case TD_SINGLE_TAP: unregister_code(KC_LNG1); break;
+//         case TD_SINGLE_HOLD: layer_off(3); break;
+//         case TD_DOUBLE_TAP: unregister_code(KC_LNG2); break;
+//         case TD_DOUBLE_HOLD: unregister_code(KC_LSFT); break;
+//         // case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_X); break;
+//         default: break;
+//     }
+//     kana_tap_state.state = TD_NONE;
+// }
+//
+// static td_tap_t tab_tap_state = {
+//     .is_press_action = true,
+//     .state = TD_NONE
+// };
+//
+// void tab_finished(tap_dance_state_t *state, void *user_data) {
+//     tab_tap_state.state = cur_dance(state);
+//     switch (tab_tap_state.state) {
+//         case TD_SINGLE_TAP: register_code(KC_TAB); break;
+//         case TD_SINGLE_HOLD: layer_on(1); break;
+//         case TD_DOUBLE_TAP: register_code(KC_ESC); break;
+//         case TD_DOUBLE_HOLD: register_code(KC_LCTL); break;
+//         // Last case is for fast typing. Assuming your key is `f`:
+//         // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+//         // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+//         // case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code(KC_X); break;
+//         default: break;
+//     }
+// }
+//
+// void tab_reset(tap_dance_state_t *state, void *user_data) {
+//     switch (tab_tap_state.state) {
+//         case TD_SINGLE_TAP: unregister_code(KC_TAB); break;
+//         case TD_SINGLE_HOLD: layer_off(1); break;
+//         case TD_DOUBLE_TAP: unregister_code(KC_ESC); break;
+//         case TD_DOUBLE_HOLD: unregister_code(KC_LCTL); break;
+//         // case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_X); break;
+//         default: break;
+//     }
+//     tab_tap_state.state = TD_NONE;
+// }
+//
+// tap_dance_action_t tap_dance_actions[] = {
+//     [TD_KANA_HAEN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, kana_finished, kana_reset),
+//     [TD_TAB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tab_finished, tab_reset)
+// };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -154,7 +165,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , MT(MOD_LCTL,KC_MINS),
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , MT(MOD_LSFT,KC_SLSH),
-    KC_LCTL  , KC_LALT  , KC_LGUI  ,TD(TD_TAB),LT(2,KC_SPC),TD(TD_KANA_HAEN),MT(MOD_LSFT,KC_BSPC),LT(1,KC_ENT), _______ , _______  , _______ , TG(1)
+    KC_LCTL  , KC_LALT  , KC_LGUI  , LT(1,KC_TAB), LT(2,KC_SPC),LT(3,KC_ESC),KC_LSFT,LT(1,KC_ENT), _______ , _______  , _______ , TG(1)
+    //KC_LCTL  , KC_LALT  , KC_LGUI  ,TD(TD_TAB),LT(2,KC_SPC),TD(TD_KANA_HAEN),MT(MOD_LSFT,KC_BSPC),LT(1,KC_ENT), _______ , _______  , _______ , TG(1)
   ),
 
   [1] = LAYOUT_universal(
